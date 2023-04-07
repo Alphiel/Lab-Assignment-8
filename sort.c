fbc3c3f4d4b83b44cb8111c9d104f41878880c29
Alan <alan.phan@knights.ucf.edu>
2023-04-07 16:10:18 -0400

    Lab 8 completed

--- sort.c
+++ sort.c
@@ -1,20 +1,114 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
+void heapify(int arr[], int N, int i)
+{
+    // variables for max heap
+	int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+	// if root < left child, note
+    if (left < N && arr[left] > arr[largest])
+        largest = left;
+ 
+	// if root < right child, note
+    if (right < N && arr[right] > arr[largest])
+        largest = right;
+ 
+	// if largest has changed, swap
+    if (largest != i) {
+        int temp = arr[i];
+		arr[i] = arr[largest];
+		arr[largest] = temp;
+
+		// repeat until heap is proper
+        heapify(arr, N, largest);
+    }
+}
+ 
 void heapSort(int arr[], int n)
 {
+    // heapify the array
+	for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+ 
+	// root of max heap is removed and placed at the end of array
+    for (int i = n - 1; i >= 0; i--) {
+		int temp = arr[0];
+		arr[0] = arr[i];
+		arr[i] = temp;
+        
+		// heapify again (root becomes largest)
+        heapify(arr, i, 0);
+    }
 }
-
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+	if (l < r) {
+		int m = (l + r) / 2;
+		// sorts left side
+		mergeSort(pData, l, m);
+		// sorts right side
+		mergeSort(pData, m + 1, r);
+		
+		int i, j, k;
+		// # terms in left side
+		int n1 = m - l + 1;
+		// # terms in right side
+		int n2 = r - m;
+
+		// create separate arrays to sort
+		int *L = (int*) malloc(n1 * sizeof(int));
+		int *R = (int*) malloc(n2 * sizeof(int));
+		extraMemoryAllocated = (n1 + n2);
+
+		// copy data into new arrays
+		for (i = 0; i < n1; i++)
+			L[i] = pData[l + i];
+		for (j = 0; j < n2; j++)
+			R[j] = pData[m + 1+ j];
+
+		i = 0;
+		j = 0;
+		k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				pData[k] = L[i];
+				i++;
+			}
+			else {
+				pData[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		// copy remaining elements
+		while (i < n1) {
+			pData[k] = L[i];
+			i++;
+			k++;
+		}
+		while (j < n2) {
+			pData[k] = R[j];
+			j++;
+			k++;
+		}
+
+		// free allocated memory
+		free(L);
+		free(R);
+	}
 }
 
 // parses input file to an integer array
